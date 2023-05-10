02d86e70a47b885a7b7ef5019e178098884794a6
Kerembozmaz <84067331+Kerembozmaz@users.noreply.github.com>
2023-05-10 23:41:30 +0300

    login oluşrturuldu

--- chat-app/src/pages/Login.jsx
+++ chat-app/src/pages/Login.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
-
+import İnput from '../components/İnput'
+import {Link} from 'react-router-dom'
+import './style.css'
 export default function Login() {
   return (
-    <div>Register</div>
+    <div className='container'>
+      <div className="form-login ">
+      <h1 className='title-login'>Neos Chat</h1>
+        <h2 className='title-form '>Giriş Yap</h2>
+        <İnput type='email' placeholder='Kullanıcı Adı veya E-mail'/>
+        <İnput type='password' placeholder='Şifre'/>
+        <div className='forgot-pass w-75 mt-2'>
+        <Link to='/Login'>Şifremi Unuttum</Link>
+        </div>
+        <div className='buton'>
+        <button className='btn-register mt-4 mb-2'>Giriş Yap</button>
+    </div>
+      </div>  
+    </div>
   )
 }
